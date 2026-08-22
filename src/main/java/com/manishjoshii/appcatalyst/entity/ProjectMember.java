5bd3e8b9a1ca724864e6a0693df7445134f70a9e
Manish Joshi <manishjoshi073@gmail.com>
2026-08-22 18:15:48 +0530

    feat(entity): add user and project management domain models

---
+++ src/main/java/com/manishjoshii/appcatalyst/entity/ProjectMember.java
@@ -0,0 +1,26 @@
+package com.manishjoshii.appcatalyst.entity;
+
+import com.manishjoshii.appcatalyst.enums.ProjectRole;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ProjectMember {
+
+    ProjectMemberId id;
+
+    Project project;
+
+    User user;
+
+    ProjectRole projectRole;
+
+    Instant invitedAt;
+    Instant acceptedAt;
+}
