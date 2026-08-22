70afdc05240e863465c4d921c6a4e7735084c631
Manish Joshi <manishjoshi073@gmail.com>
2026-08-22 17:48:30 +0530

    feat: initialize Spring Boot main application and test context

---
+++ src/main/java/com/manishjoshii/appcatalyst/AppcatalystApplication.java
@@ -0,0 +1,13 @@
+package com.manishjoshii.appcatalyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcatalystApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcatalystApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manishjoshii/appcatalyst/AppcatalystApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manishjoshii.appcatalyst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcatalystApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
