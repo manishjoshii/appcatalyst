c7ac549c4b035b4fae96b19887be708f3b792da2
Manish Joshi <manishjoshi073@gmail.com>
2026-08-31 12:50:35 +0530

    feat(subscription): define Subscription entity, repository, and response DTO

--- src/main/java/com/manishjoshii/appcatalyst/entity/Subscription.java
+++ src/main/java/com/manishjoshii/appcatalyst/entity/Subscription.java
@@ -1,31 +1,48 @@
 package com.manishjoshii.appcatalyst.entity;
 
 import com.manishjoshii.appcatalyst.enums.SubscriptionStatus;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
 
 @Getter
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Subscription {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "user_id")
     User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false, name = "plan_id")
     Plan plan;
 
+    @Enumerated(value = EnumType.STRING)
     SubscriptionStatus status;
 
-    String stripeSubscriptionId;
+    String stripeSubscriptionId; //can be renamed to gatewaySubscriptionId
 
     Instant currentPeriodStart;
     Instant currentPeriodEnd;
+    @Builder.Default
     Boolean cancelAtPeriodEnd = false;
 
+    @CreationTimestamp
     Instant createdAt;
+
+    @UpdateTimestamp
     Instant updatedAt;
 }
--- src/main/java/com/manishjoshii/appcatalyst/repository/PlanRepository.java
+++ src/main/java/com/manishjoshii/appcatalyst/repository/PlanRepository.java
@@ -3,5 +3,8 @@ package com.manishjoshii.appcatalyst.repository;
 import com.manishjoshii.appcatalyst.entity.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlanRepository extends JpaRepository<Plan, Long> {
+    Optional<Plan> findByStripePriceId(String id);
 }
